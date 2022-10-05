6a6f0f7ca57df5a35896ae9570f3bb143c86b91d
Bidz42 <abid97r@gmail.com>
2022-10-05 18:59:20 +0100

    icons done

--- src/components/icons.js
+++ src/components/icons.js
@@ -1,19 +1,65 @@
-import React from 'react'
+import React from "react";
 
 const Icons = () => {
   return (
-    <div class="flex flex-col">
+    <div class="h-full">
+      <h1 class="text-center align-middle text-4xl mb-10">WHAT WE DO</h1>
 
-    <h1 class="text-center align-middle text-4xl">WHAT WE DO</h1>
+      <div class="flex flex-col md:flex-row md:justify-center ml-20 md:ml-0 p-10 text-center ">
+        <div class="w-40 m-4 lg:m-10">
+          <img
+            src="https://www.punkt-vor-strich.at/assets/images/pvs_mobilitaetsanalyse.jpeg"
+            alt="logo"
+            class="w-30"
+          />
+          <h1 class="text-lg font-bold my-4">Mobilitätsanalyse</h1>
+          <p  class="text-base">
+            Wir sammeln, analysieren und visualisieren Mobilitätsdaten um die
+            Diversität in der Mobilität sichtbar zu machens
+          </p>
+        </div>
 
-    <div>
-      <img src="https://www.punkt-vor-strich.at/assets/images/pvs_mobilitaetsanalyse.jpeg" alt="logo" class="object-scale-down w-40" />
-      <h1>Mobilitätsanalyse</h1>
-      <p>Wir sammeln, analysieren und visualisieren Mobilitätsdaten um die Diversität in der Mobilität sichtbar zu machens</p>
+        <div class="w-40 m-4 lg:m-10">
+          <img
+            src="https://www.punkt-vor-strich.at/assets/images/pvs_mobilitaetsanalyse.jpeg"
+            alt="logo"
+            class="w-30"
+          />
+          <h1 class="text-lg font-bold my-4">Co-Creation</h1>
+          <p  class="text-base">
+            Durch den Einsatz von innovativen Methoden wie Service Design und
+            Open Innovation, schaffen wir eine bessere Customer Experience
+          </p>
+        </div>
+
+        <div class="w-40 m-4 lg:m-10">
+          <img
+            src="https://www.punkt-vor-strich.at/assets/images/pvs_mobilitaetsanalyse.jpeg"
+            alt="logo"
+            class="w-30"
+          />
+          <h1 class="text-lg font-bold my-4">Beratung</h1>
+          <p  class="text-base">
+            Wir beraten von der Erhebung von Bewegungbaseustern und Anforderungen
+            bis hin zum Anstoß der Umsetzung bedarfsgerechter Mobilität
+          </p>
+        </div>
+
+        <div class="w-40 m-4 lg:m-10">
+          <img
+            src="https://www.punkt-vor-strich.at/assets/images/pvs_mobilitaetsanalyse.jpeg"
+            alt="logo"
+            class="w-30"
+          />
+          <h1 class="text-lg font-bold my-4">Bewusstseinsbildung</h1>
+          <p  class="text-base">
+            Durch die Sichtbarmachung von Daten wird besseres Bewusstsein
+            geschaffen und fundiertere Entscheidungen getroffen
+          </p>
+        </div>
+      </div>
     </div>
+  );
+};
 
-    </div>
-  )
-}
-
-export default Icons
+export default Icons;
