84b63ff4bad534aaebb587511a7be804429ea94e
Bidz42 <abid97r@gmail.com>
2022-10-07 20:35:07 +0100

    Blog delete not working

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -21,9 +21,7 @@ const Blog = () => {
   useEffect(() => {
     axios
       .get("http://localhost:5005/blog")
-      .then((response) => {
-        console.log(response.data)
-        setBlogPostList(response.data)})
+      .then((response) => setBlogPostList(response.data))
       .catch((err) => console.log(err));
   }, [refresh]);
 
@@ -59,6 +57,13 @@ const Blog = () => {
       .then((response) => console.log(response))
       .catch((err) => console.log(err));
   };
+
+  const handleDelete = (e, id) => {
+   e.preventDefault()
+    axios.delete("http://localhost:5005/blog/delete", {id} )
+    .then(response => console.log(response))
+    .catch(err => console.log(err))
+  }
 
   
 
@@ -131,13 +136,20 @@ const Blog = () => {
             >
 
               {isLoggedIn && 
+           
+              <div className="flex flex-row">
               <svg onClick={() => setOpenEdit(!openEdit)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="hover:cursor-pointer relative w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
-              </svg>            
+              </svg>
+              <svg onClick={(e) => handleDelete(e, blog?._id)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="mx-10 hover:cursor-pointer relative w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+              </svg>
+              </div>
+                      
               }
                 {openEdit &&
                 <>
-                <form onSubmit={ (e) => handleEdit(e, blog._id)}>
+                <form onSubmit={ (e) => handleEdit(e, blog?._id)}>
                 <h1 className="text-lg py-2">Posted by:<input placeholder={blog.author} onChange={(e) => setAuthor(e.target.value)} className='text-black border-pink border-2' /></h1>
                 <h2 className="text-base py-2">Subject: <input placeholder={blog.subject} onChange={(e) => setSubject(e.target.value)} className='text-black border-pink border-2' /></h2>
                 <p className="text-xs leading-5"><textarea placeholder={blog.message} onChange={(e) => setMessage(e.target.value)} className='text-black border-pink border-2 w-full' /></p>
