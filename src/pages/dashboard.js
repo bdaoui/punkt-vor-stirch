5ca23df8d88529e85087a8c23a410bcca0cfd872
Bidz42 <abid97r@gmail.com>
2022-10-08 22:41:33 +0100

    skeleton guiden on dahsboard

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -12,6 +12,22 @@ const Dashboard = () => {
 
   // Timetable / Calendar 
 
+  //skeleton done. tabs should work with href change to hashlink of component to render. so rest
+  //are hidden and selected is shown. Can also e done with state
+  //const [openTab, setOpenTab] = useState(1) something like this 
+  //then each a tag will have function like 
+  //  <a
+  //    className={"blah blah blah " + (openTab === 1 ? "text-white bg-" + color + "-600" : "text-" + color + "-600 bg-white")}
+  //      onClick={e => {e.preventDefault();  setOpenTab(1); }}
+  //      data-toggle="tab" href="#link1" or Link to="#link1"role="tablist" >
+  //      
+  //then we can set our maps like so 
+  //  <div className={openTab === 1 ? "block" : "hidden"} id="link1">
+  //    {blahblah.map()..........}
+  //   <div className={openTab === 2 ? "block" : "hidden"} id="link2">
+  //    {blahblah.map()..........}
+
+
 
   return (
     <div className='h-screen'>
@@ -28,19 +44,19 @@ const Dashboard = () => {
 </div>
 <ul class="hidden text-sm font-medium text-center text-white rounded-lg divide-x divide-gray-200 shadow sm:flex ">
     <li class="w-full">
-        <a href="#" class="inline-block p-4 my-1 w-full bg-pink focus:ring-2 focus:ring-black hover:text-gray-400 hover:bg-pink/75 active rounded" aria-current="page">Unread</a>
+        <a href="#" class="inline-block p-4 my-1 w-full bg-pink focus:ring-4 focus:ring-pink  focus:text-pink focus:bg-white hover:text-gray-400 hover:bg-pink/75 active rounded" aria-current="page">Unread</a>
     </li>
     <li class="w-full">
-        <a href="#" class="inline-block p-4 my-1 w-full bg-pink focus:ring-2 focus:ring-black hover:text-gray-400 hover:bg-pink/75 rounded">Read</a>
+        <a href="#" class="inline-block p-4 my-1 w-full bg-pink focus:ring-4 focus:ring-pink  focus:text-pink focus:bg-white hover:text-gray-400 hover:bg-pink/75 rounded">Read</a>
     </li>
     <li class="w-full">
-        <a href="#" class="inline-block p-4 my-1 w-full bg-pink focus:ring-2  focus:ring-black hover:text-gray-400 hover:bg-pink/75 rounded">Resolved</a>
+        <a href="#" class="inline-block p-4 my-1 w-full bg-pink  focus:ring-4 focus:ring-pink  focus:text-pink focus:bg-white hover:text-gray-400 hover:bg-pink/75 rounded">Resolved</a>
     </li>
     <li class="w-full">
-        <a href="#" class="inline-block p-4 my-1 w-full bg-pink focus:ring-2 focus:ring-black hover:text-gray-400 hover:bg-pink/75 rounded">Unresolved</a>
+        <a href="#" class="inline-block p-4 my-1 w-full bg-pink focus:ring-4 focus:ring-pink  focus:text-pink focus:bg-white hover:text-gray-400 hover:bg-pink/75 rounded">Unresolved</a>
     </li>
     <li class="w-full">
-        <a href="#" class="inline-block p-4 my-1 w-full bg-pink focus:ring-2 focus:ring-black hover:text-gray-400 hover:bg-pink/75 rounded">Important</a>
+        <a href="#" class="inline-block p-4 my-1 w-full bg-pink focus:ring-4 focus:ring-pink  focus:text-pink focus:bg-white hover:text-gray-400 hover:bg-pink/75 rounded">Important</a>
     </li>
 </ul>
      
