274d925c3810c28b0e0f325b6a46c589b83b72aa
Bidz42 <abid97r@gmail.com>
2022-10-11 13:05:32 +0100

    protect roues

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React from 'react'
+import React, { useContext } from 'react'
 import { Route, Routes} from 'react-router-dom'
 import Impressum from './pages/impressum';
 import Landing from './pages/landing';
@@ -7,8 +7,11 @@ import Blog from './pages/blog'
 import Login from './components/login';
 import Dashboard from './pages/dashboard';
 import ConfigOutlet from './context/configOutlet';
+import {AuthContext} from './context/auth.context';
 
 function App() {
+
+  const {isLoggedIn} = useContext(AuthContext)
   return (
     <div className='App font-display'>
     <Routes>
@@ -17,7 +20,9 @@ function App() {
           <Route path='/impressum' element={<Impressum />} />
           <Route path='/blog' element={<Blog />} />
           <Route path="/admin/login" element={<Login />} />
+{isLoggedIn &&
           <Route path="/admin/dashboard" element={<Dashboard />} />
+}
         </Route>
     </Routes>
     </div>
