6d346f5084c78263f954c4d38d4d4d14f855fa7b
Bidz42 <abid97r@gmail.com>
2022-10-11 13:02:23 +0100

    link with a's

--- src/components/partners.js
+++ src/components/partners.js
@@ -11,17 +11,17 @@ const Partners = () => {
       </div>
       
       <div className='flex flex-col md:flex-row justify-center'>
-        <Link to="https://www.womeninmobility.org/"> <img className='object-scale-down h-20 my-4 md:mx-6' src ="https://www.punkt-vor-strich.at/assets/images/wim_Logo.png"
-        alt="logo" /></Link>
+        <a target="_blank" href="https://www.womeninmobility.org/"> <img className='object-scale-down h-20 my-4 md:mx-6' src ="https://www.punkt-vor-strich.at/assets/images/wim_Logo.png"
+        alt="logo" /></a>
 
-        <Link to="https://triply.at/"><img className='object-scale-down h-20 my-4 md:mx-6' src ="https://www.punkt-vor-strich.at/assets/images/triply_Logo.png"
-        alt="logo" /></Link>
-        <Link to="https://www.plus.ac.at/geoinformatik/"><img className='object-scale-down h-10 my-4 md:mt-6 md:mx-6' src ="https://www.punkt-vor-strich.at/assets/images/zgis_logo.png"
-        alt="logo" /></Link>
-        <Link to="https://www.carployee.com/"><img className='object-scale-down h-10 my-4 md:mt-5 md:mx-6' src ="https://www.punkt-vor-strich.at/assets/images/carployee_logo.png"
-        alt="logo" /></Link>
-        <Link to="https://www.ffg.at/"><img className='object-scale-down h-20 my-4 md:mt-2 md:mx-6' src ="https://www.punkt-vor-strich.at/assets/images/FFG_Logo.png"
-        alt="logo" /></Link>
+        <a target="_blank" href="https://triply.at/"><img className='object-scale-down h-20 my-4 md:mx-6' src ="https://www.punkt-vor-strich.at/assets/images/triply_Logo.png"
+        alt="logo" /></a>
+        <a target="_blank" href="https://www.plus.ac.at/geoinformatik/"><img className='object-scale-down h-10 my-4 md:mt-6 md:mx-6' src ="https://www.punkt-vor-strich.at/assets/images/zgis_logo.png"
+        alt="logo" /></a>
+        <a target="_blank" href="https://www.carployee.com/"><img className='object-scale-down h-10 my-4 md:mt-5 md:mx-6' src ="https://www.punkt-vor-strich.at/assets/images/carployee_logo.png"
+        alt="logo" /></a>
+        <a target="_blank" href="https://www.ffg.at/"><img className='object-scale-down h-20 my-4 md:mt-2 md:mx-6' src ="https://www.punkt-vor-strich.at/assets/images/FFG_Logo.png"
+        alt="logo" /></a>
       </div>
 
 
