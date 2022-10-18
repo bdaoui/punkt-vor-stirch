3df74c49e7c7b08bdef3be4efd82dd4482766f7d
Soufiane Bdaoui <soufianebdaoui@gmail.com>
2022-10-18 22:26:27 +0200

    deconstructing isLoggedIn

--- src/components/team.js
+++ src/components/team.js
@@ -5,7 +5,7 @@ import { AuthContext  } from "../context/auth.context";
 
 const Team = () => {
 
-  const isLoggedIn = useContext(AuthContext)
+  const {isLoggedIn} = useContext(AuthContext)
 
   const [showSocial, setShowSocial] = useState([false, ""]);
   const [name, setName] = useState("");
